cb1f7a9eba9575ea5aec76ea990f1b9675c90faa
Muhammad Syauqi Amiq Amrullah <syauqiamiq12@gmail.com>
2022-01-28 12:56:38 +0700

    fix: Footer Responsive Issue

--- components/Footer/FooterComponent.jsx
+++ components/Footer/FooterComponent.jsx
@@ -14,88 +14,66 @@ const FooterComponent = () => {
         <Container>
           <Row>
             <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <h3 className={styles.name}>SyauqiAmiq</h3>
+              <div className={styles.contentWrapper}>
+                <h3 className={styles.name}>SyauqiAmiq</h3>
+              </div>
             </Col>
             <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <h3 className={styles.routeActive}>Navigation</h3>
+              <div className={styles.contentWrapper}>
+                <Link href="/">
+                  <a className={router.asPath === "/" ? styles.routeActive : styles.route}>Home</a>
+                </Link>
+                <Link href="/about">
+                  <a className={router.asPath === "/about" ? styles.routeActive : styles.route}>Tentang Saya</a>
+                </Link>
+                <Link href="/projects">
+                  <a className={router.asPath === "/projects" ? styles.routeActive : styles.route}>Projects</a>
+                </Link>
+              </div>
             </Col>
             <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <h3 className={styles.routeActive}>Hubungi Saya</h3>
+              <div className={styles.contentWrapper}>
+                <h3 className={styles.title}>Hubungi Saya</h3>
+                <Link href="/">
+                  <a className={styles.route}>Whatsapp</a>
+                </Link>
+                <Link href="/">
+                  <a className={styles.route}>Email</a>
+                </Link>
+                <Link href="/">
+                  <a className={styles.route}>Linkedin</a>
+                </Link>
+              </div>
             </Col>
             <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <h3 className={styles.routeActive}>Social Media</h3>
+              <div className={styles.contentWrapper}>
+                <h3 className={styles.title}>Social Media</h3>
+                <Link href="/">
+                  <a className={styles.iconWrapper}>
+                    {" "}
+                    <FontAwesomeIcon icon={faFacebook} className={styles.socialIcon} />
+                  </a>
+                </Link>
+                <Link href="/">
+                  <a className={styles.iconWrapper}>
+                    {" "}
+                    <FontAwesomeIcon icon={faInstagram} className={styles.socialIcon} />
+                  </a>
+                </Link>
+                <Link href="/">
+                  <a className={styles.iconWrapper}>
+                    {" "}
+                    <FontAwesomeIcon icon={faLinkedinIn} className={styles.socialIcon} />
+                  </a>
+                </Link>
+                <Link href="/">
+                  <a className={styles.iconWrapper}>
+                    {" "}
+                    <FontAwesomeIcon icon={faGithub} className={styles.socialIcon} />
+                  </a>
+                </Link>
+              </div>
             </Col>
-          </Row>
-
-          <Row>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              {" "}
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <Link href="/">
-                <a className={router.asPath === "/" ? styles.routeActive : styles.route}>Home</a>
-              </Link>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <Link href="/">
-                <a className={styles.route}>Whatsapp</a>
-              </Link>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <Link href="/">
-                <a className={styles.route}>
-                  {" "}
-                  <FontAwesomeIcon icon={faFacebook} className={styles.socialIcon} />
-                </a>
-              </Link>
-              <Link href="/">
-                <a className={styles.route}>
-                  {" "}
-                  <FontAwesomeIcon icon={faInstagram} className={styles.socialIcon} />
-                </a>
-              </Link>
-              <Link href="/">
-                <a className={styles.route}>
-                  {" "}
-                  <FontAwesomeIcon icon={faLinkedinIn} className={styles.socialIcon} />
-                </a>
-              </Link>
-              <Link href="/">
-                <a className={styles.route}>
-                  {" "}
-                  <FontAwesomeIcon icon={faGithub} className={styles.socialIcon} />
-                </a>
-              </Link>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}></Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <Link href="/about">
-                <a className={router.asPath === "/about" ? styles.routeActive : styles.route}>Tentang Saya</a>
-              </Link>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <Link href="/">
-                <a className={styles.route}>Email</a>
-              </Link>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}></Col>
-          </Row>
-          <Row>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}></Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              {" "}
-              <Link href="/projects">
-                <a className={router.asPath === "/projects" ? styles.routeActive : styles.route}>Projects</a>
-              </Link>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}>
-              <Link href="/">
-                <a className={styles.route}>Linkedin</a>
-              </Link>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={3} xl={3}></Col>
           </Row>
         </Container>
         <Container>
